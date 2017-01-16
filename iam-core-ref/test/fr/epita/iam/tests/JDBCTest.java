2a6aecbb9f5d36d1a42dabfd1faaee59637d075d
tbrou <tbrou@tbrou-PC7.mshome.net>
2017-01-16 20:15:54 +0100

    * select / insert from jdbc

--- iam-core-ref/test/fr/epita/iam/tests/JDBCTest.java
+++ iam-core-ref/test/fr/epita/iam/tests/JDBCTest.java
@@ -8,6 +8,11 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import fr.epita.iam.datamodel.Identity;
 
 /**
  * @author tbrou
@@ -22,15 +27,60 @@ public class JDBCTest {
 		Connection connection = DriverManager.getConnection("jdbc:derby://localhost:1527/IAM;create=true","TOM","TOM");
 		System.out.println(connection.getSchema());
 		
+		List<Identity> identities = listAllIdentities(connection);
+		System.out.println("Identities : " + identities);
 		
-		PreparedStatement pstmt = connection.prepareStatement("select * from IDENTITIES");
-		ResultSet rs = pstmt.executeQuery();
-		while (rs.next()){
-			System.out.println(rs.getString("IDENTITIES_DISPLAYNAME"));
-		}
+		boolean success = insertIdentity(connection, new Identity(null, "quentin decayeux", "qdeca@qdeca.com"));
+		System.out.println("got this return from the database : "+ success);
+		
+		
+		identities = listAllIdentities(connection);
+		System.out.println("Identities After insert: " + identities);
+		
+		
+		
 		
 		connection.close();
 		
+	}
+
+	/**
+	 * @param connection
+	 * @return
+	 * @throws SQLException
+	 */
+	private static boolean insertIdentity(Connection connection, Identity identity) throws SQLException {
+		String insertStatement = 
+				"insert into IDENTITIES (IDENTITIES_DISPLAYNAME, IDENTITIES_EMAIL, IDENTITIES_BIRTHDATE) "
+				+ "values(?, ?, ?)";
+		PreparedStatement pstmt_insert = connection.prepareStatement(insertStatement);
+		pstmt_insert.setString(1, identity.getDisplayName());
+		pstmt_insert.setString(2, identity.getEmail());
+		Date now = new Date();
+		pstmt_insert.setDate(3,new java.sql.Date(now.getTime()));
+		
+		boolean success = pstmt_insert.execute();
+		return success;
+	}
+
+	/**
+	 * @param connection
+	 * @throws SQLException
+	 */
+	private static List<Identity>  listAllIdentities(Connection connection) throws SQLException {
+		List<Identity> identities = new ArrayList<Identity>();
+		
+		PreparedStatement pstmt_select = connection.prepareStatement("select * from IDENTITIES");
+		ResultSet rs = pstmt_select.executeQuery();
+		while (rs.next()){
+			String displayName = rs.getString("IDENTITIES_DISPLAYNAME");
+			String uid = String.valueOf(rs.getString("IDENTITIES_UID"));
+			String email = rs.getString("IDENTITIES_EMAIL");
+			Date birthDate = rs.getDate("IDENTITIES_BIRTHDATE");
+			Identity identity = new Identity(uid, displayName, email);
+			identities.add(identity);
+		}		
+		return identities;
 	}
 
 }
